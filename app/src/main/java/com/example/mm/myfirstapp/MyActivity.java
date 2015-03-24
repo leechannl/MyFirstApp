e408ce2dfbc9173bf997ccf52a91ae9387cf96c6
leechannl <leechannl@gmail.com>
2015-03-24 18:28:53 +0800

    Add buttons to action bar, not finished yet.

--- app/src/main/java/com/example/mm/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/mm/myfirstapp/MyActivity.java
@@ -3,6 +3,7 @@ package com.example.mm.MyFirstApp;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,7 +31,7 @@ public class MyActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -38,13 +39,15 @@ public class MyActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Log.i("com.mm", "search button pressed");
+                return true;
+            case R.id.action_settings:
+                Log.i("com.mm", "setting button pressed");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
